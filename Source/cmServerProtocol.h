bfd77b943a46ef2769b27c09ef5dacffda52fe99
Tobias Hunger <tobias.hunger@qt.io>
2016-05-30 16:07:24 +0200

    daemon-mode: Automate progress reporting
    Wire up cmake::SetProgressUpdate to the progress reporting of the
    cmake metadata server.

--- Source/cmServerProtocol.h
+++ Source/cmServerProtocol.h
@@ -58,9 +58,6 @@ private:
 class cmServerRequest
 {
 public:
-  void ReportProgress(int min, int current, int max,
-                      const std::string& message) const;
-
   cmServerResponse Reply(const Json::Value& data) const;
   cmServerResponse ReportError(const std::string& message) const;
 
@@ -71,6 +68,9 @@ public:
 private:
   cmServerRequest(cmServer* server, const std::string& t, const std::string& c,
                   const Json::Value& d);
+
+  void ReportProgress(int min, int current, int max,
+                      const std::string& message) const;
 
   cmServer* m_Server;
 
@@ -98,6 +98,8 @@ protected:
 
 private:
   std::unique_ptr<cmake> m_CMakeInstance;
+
+  friend class cmServer;
 };
 
 class cmServerProtocol0_1 : public cmServerProtocol
