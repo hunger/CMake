b3a3a7e68de12ccf3fa0bb19d58018d024eb1f08
Tobias Hunger <tobias.hunger@qt.io>
2016-09-09 10:01:44 +0200

    server-mode: Move constants for server mode into its own file
    This removes some duplication and makes it easier to reuse the
    vocabulary.

---
+++ Source/cmServerDictionary.h
@@ -0,0 +1,30 @@
+/*============================================================================
+  CMake - Cross Platform Makefile Generator
+  Copyright 2016 Tobias Hunger <tobias.hunger@qt.io>
+
+  Distributed under the OSI-approved BSD License (the "License");
+  see accompanying file Copyright.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+============================================================================*/
+
+namespace {
+// Vocabulary:
+
+const char ERROR_TYPE[] = "error";
+const char MESSAGE_TYPE[] = "message";
+const char PROGRESS_TYPE[] = "progress";
+const char REPLY_TYPE[] = "reply";
+
+const char BUILD_DIRECTORY_KEY[] = "buildDirectory";
+const char COOKIE_KEY[] = "cookie";
+const char ERROR_MESSAGE_KEY[] = "errorMessage";
+const char EXTRA_GENERATOR_KEY[] = "extraGenerator";
+const char GENERATOR_KEY[] = "generator";
+const char REPLY_TO_KEY[] = "inReplyTo";
+const char SOURCE_DIRECTORY_KEY[] = "sourceDirectory";
+const char TYPE_KEY[] = "type";
+
+} // namespace
